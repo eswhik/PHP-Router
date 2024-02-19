7fa5c63c68506a37d10440a4ffe835aba5361372
José Caruajulca <159473753+TheEswhik@users.noreply.github.com>
2024-02-19 17:15:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+class Router
+{
+    private $routes;
+
+    public function __construct(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function handleRequest()
+    {
+        $url = $this->sanitizeUrl($_GET['url'] ?? '/');
+
+        if ($this->isValidUrl($url)) {
+            $this->routeRequest($url);
+        } else {
+            $this->showError('Invalid URL');
+        }
+    }
+
+    private function sanitizeUrl($url)
+    {
+        return htmlspecialchars($url);
+    }
+
+    private function isValidUrl($url)
+    {
+        return preg_match('/^[a-zA-Z0-9\/]+$/', $url);
+    }
+
+    private function routeRequest($url)
+    {
+        if (array_key_exists($url, $this->routes)) {
+            list($folder, $file) = array_map('htmlspecialchars', explode('/', $this->routes[$url], 2));
+            $filePath = 'app/' . $folder . '/' . $file;
+
+            $this->includeFile($filePath);
+        } else {
+            $this->showError('404 Not Found');
+        }
+    }
+
+    private function includeFile($filePath)
+    {
+        if (file_exists($filePath)) {
+            include $filePath;
+        } else {
+            $this->showError('404 Not Found');
+        }
+    }
+
+    private function showError($message)
+    {
+        echo $message;
+        exit;
+    }
+}
+
+$routes = [
+    'back/login' => 'modules/auth/login.php',
+    'back/registro' => 'modules/auth/registro.php',
+    'back/add' => 'modules/products/add.php',
+    'home/' => 'views/home.php',
+    '/' => 'views/index.php',
+];
+
+$router = new Router($routes);
+$router->handleRequest();
